5b012a4802b54e068a5cb75b7252701f1388a9dc
Sean Hayes <hayessea000@outlook.com>
2023-05-01 18:08:07 -0500

    finished ids

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.post('/api/notes', (req, res) => {
       const newNote = {
         title,
         text,
-        note_id: uuid(),
+        id: uuid(),
     };
       
     const reviewString = JSON.stringify(newNote);
