79ba57d2632cfc2ff5b9e7036bd5814d483a4929
Sean Hayes <hayessea000@outlook.com>
2023-05-01 17:56:20 -0500

    added id maker

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const dbData = require('./db/db.json');
-
+const uuid = require('./helpers/uuid');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -30,29 +30,40 @@ app.post('/api/notes', (req, res) => {
       const newNote = {
         title,
         text,
-        // note_id: uuid(),
-      };
+        note_id: uuid(),
+    };
       
-      const reviewString = JSON.stringify(newNote);
+    const reviewString = JSON.stringify(newNote);
 
-      fs.writeFile(`./db/${newNote}.json`, reviewString, (err) =>
-      err
-        ? console.error(err)
-        : console.log(
-            `Note for ${newNote} has been written to JSON file`
-          )
-      );
-      const response = {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const parsedNotes = JSON.parse(data);
+  
+          parsedNotes.push(newNote);
+  
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated notes!')
+          );
+        }
+    });
+    const response = {
         status: 'success',
         body: newNote,
-      };
+    };
   
-      console.log(response);
-      res.status(201).json(response);
+    console.log(response);
+    res.status(201).json(response);
     } else {
-      res.status(500).json('Error in posting review');
+      res.status(500).json('Error in posting note');
     }
-  });
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
