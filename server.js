80ade02aba0a3653ef71cd842125d93fdb9f7f82
Sean Hayes <hayessea000@outlook.com>
2023-05-01 15:40:13 -0500

    loads up the html pages

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const util = require('util');
+// const util = require('util');
 
 const PORT = process.env.PORT || 3001;
+
+const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(express.static('public'));
 
-const app = express();
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
